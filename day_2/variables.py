409c622e4019f30704c85f12251c256ccfc600cc
millenniumlurker <saraleite.mail@gmail.com>
2025-08-06 23:07:47 +0200

    Day 2: Variables, Builtin Functions

---
+++ day_2/variables.py
@@ -0,0 +1,61 @@
+# Day 2: 30 Days of Python programming
+
+#Exercises: Level 1
+first_name = 'Sara'
+last_name = 'Leite'
+full_name = 'Sara Leite'
+country = 'Spain'
+city = 'Madrid'
+age = 999
+year = 2025
+is_married = False
+is_true = True
+is_light_on = False
+
+is_tv_on, studying, working = False, True, True
+
+# Exercises: Level 2
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(is_tv_on))
+print(type(studying))
+print(type(working))
+
+print(len(first_name))
+
+print(len(first_name) < len(last_name))
+
+num_one = 5
+num_two = 4
+
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_two % num_one
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+radius = input('What is the radius: ')
+radius = int(radius)
+area_of_circle = 3.14 * (radius ** 2)
+print(area_of_circle)
+
+circum_of_circle = 2 * 3.14 * radius
+print(circum_of_circle)
+
+user_first_name = input('What is your first name? ')
+user_last_name = input('What is your last name? ')
+user_country = input('What is your country? ')
+user_age = input('What is your age? ')
+
+help('keywords')
